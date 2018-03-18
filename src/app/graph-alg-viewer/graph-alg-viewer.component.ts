dd01c8f5f2bc45976e6a9d20dc89f16eb45be73d
Warwick Stone <uozu.aho@gmail.com>
2018-03-18 15:43:49 +1100

    start state machine for alg viewer

--- src/app/graph-alg-viewer/graph-alg-viewer.component.ts
+++ src/app/graph-alg-viewer/graph-alg-viewer.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-graph-alg-viewer',
   templateUrl: './graph-alg-viewer.component.html',
   styleUrls: ['./graph-alg-viewer.component.css']
 })
-export class GraphAlgViewerComponent implements OnInit {
+export class GraphAlgViewerComponent {
 
-  constructor() { }
+  public instructions: string;
+  public nextButtonText: string;
+  public currentState: IAlgViewerState;
+  public StateTypes = StateType;
 
-  ngOnInit() {
+  constructor() {
+    this.currentState = this.createCreateGraphState();
+    this.currentState.run();
   }
 
+  public next() {
+    this.currentState = this.currentState.next(StateInput.Next);
+    this.currentState.run();
+  }
+
+  private createCreateGraphState(): IAlgViewerState {
+    const _comp = this;
+    const run = () => {
+      _comp.instructions = 'Create a graph';
+      _comp.nextButtonText = 'Done';
+    };
+    const next = input => this.dummyState();
+    return new AlgViewerState(StateType.CreatingGraph, run, next);
+  }
+
+  private dummyState(): IAlgViewerState {
+    const run = () => {
+      this.instructions = 'you win!';
+    };
+    const next = input => this.dummyState();
+    return new AlgViewerState(StateType.Dummy, run, next);
+  }
+}
+
+enum StateType {
+  CreatingGraph,
+  Dummy
+}
+
+enum StateInput {
+  Next
+}
+
+interface IAlgViewerState {
+  readonly state: StateType;
+  run(): void;
+  next(input: StateInput): IAlgViewerState;
+}
+
+/**
+ * Generic viewer state. State actions are determined by functions
+ * passed to the constructor.
+ */
+class AlgViewerState implements IAlgViewerState {
+
+  constructor(
+    public state: StateType,
+    public run: (() => void),
+    public next: ((input: StateInput) => IAlgViewerState)) {
+  }
 }
