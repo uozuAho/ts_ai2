65734db4cec00567ec0f6574dd38304b54c40737
Warwick Stone <uozu.aho@gmail.com>
2018-03-23 22:36:39 +1100

    use edmonds-blossom npm package instead

--- src/ai_lib/algorithms/graph/blossom.spec.ts
+++ src/ai_lib/algorithms/graph/blossom.spec.ts
@@ -1,9 +1,18 @@
 import { Blossom } from './blossom';
 
-describe('Blossom', function() {
-
-    it('does it build', function() {
-        const b = new Blossom();
-        b.doEverything();
+describe('Blossom2', function() {
+    it('simple', function() {
+        const edges = [
+            [0, 1, 6],  // 0 ----6----> 1
+            [0, 2, 10]  // 0 ---10----> 2
+        ];
+        const b = new Blossom(edges);
+        expect(b.hasMatch(0)).toBe(true);
+        expect(b.hasMatch(1)).toBe(false);
+        expect(b.hasMatch(2)).toBe(true);
+        expect(b.getMatch(0)).toBe(2);
+        expect(b.getMatch(1)).toBe(Blossom.NO_MATCH);
+        expect(b.getMatch(2)).toBe(0);
+        expect(b.getMatches()).toEqual([2, -1, 0]);
     });
 });
