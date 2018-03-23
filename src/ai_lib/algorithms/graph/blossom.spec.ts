e3b994470f91e4c5f770a476bb75ec76117818da
Warwick Stone <uozu.aho@gmail.com>
2018-03-23 18:39:24 +1100

    wrap in class, add 'build' test

---
+++ src/ai_lib/algorithms/graph/blossom.spec.ts
@@ -0,0 +1,9 @@
+import { Blossom } from './blossom';
+
+describe('Blossom', function() {
+
+    it('does it build', function() {
+        const b = new Blossom();
+        b.doEverything();
+    });
+});
