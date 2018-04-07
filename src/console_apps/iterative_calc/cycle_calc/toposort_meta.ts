2ff02a3893a1f490277938dfd01d16aebc748070
Warwick Stone <uozu.aho@gmail.com>
2018-04-07 20:56:49 +1000

    rename toposort_meta

--- src/console_apps/iterative_calc/cycle_calc/toposort_meta.ts
+++ src/console_apps/iterative_calc/cycle_calc/toposort_meta.ts
@@ -4,14 +4,16 @@ import { MetaNode } from './meta_node';
 import { MetaDag } from './meta_dag';
 
 /** Finds a topological order for any directed graph. */
-export class TopoSortCycle {
+export class TopoSortMeta {
 
-    /** Compute a topological order of any directed graph. Cycles are converted to MetaNodes.
-     * @returns an array of MetaNodes in a topological order
+    public order: MetaNode[];
+
+    /** Compute a topological order of any directed graph.
+     *  Cycles are converted to MetaNodes.
      */
-    public static computeOrder(graph: IGraph): MetaNode[] {
+    constructor(graph: IGraph) {
         const metaDag = new MetaDag(graph);
         const topo = new TopoSort(metaDag.graph);
-        return Array.from(topo.order()).map(idx => metaDag.nodes[idx]);
+        this.order = Array.from(topo.order()).map(idx => metaDag.nodes[idx]);
     }
 }
