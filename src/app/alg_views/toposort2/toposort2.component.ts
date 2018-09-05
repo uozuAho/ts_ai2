22409ed5bfb72664d28579b1bf77db0de616210c
Warwick Stone <uozu.aho@gmail.com>
2018-09-05 21:41:15 +1000

    draw nodes and edges using svg

--- src/app/alg_views/toposort2/toposort2.component.ts
+++ src/app/alg_views/toposort2/toposort2.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DiGraphT } from '../../../ai_lib/structures/graphT';
+import { Edge } from '../../../ai_lib/structures/igraph';
 
 @Component({
   selector: 'app-toposort2',
@@ -9,14 +10,31 @@ import { DiGraphT } from '../../../ai_lib/structures/graphT';
 export class Toposort2Component {
 
     public nodes: Node2d[];
+    public edges: Edge2d[];
     private _graph: DiGraphT<Node2d>;
 
     constructor() {
         this._graph = new DiGraphT<Node2d>();
-        this._graph.add_node(new Node2d(50, 50));
-        this._graph.add_node(new Node2d(100, 100));
-        this._graph.add_edge(0, 1);
+        for (let i = 0; i < 20; i++) {
+            this._graph.add_node(this.randomNode());
+        }
+        for (let i = 0; i < 40; i++) {
+            const from = Math.floor(Math.random() * 20);
+            const to = Math.floor(Math.random() * 20);
+            this._graph.add_edge(from, to);
+        }
         this.nodes = this._graph.get_nodes();
+        this.edges = this._graph.get_edges().map(e => this.edge22d(e));
+    }
+
+    private randomNode(): Node2d {
+        return new Node2d(Math.random() * 500, Math.random() * 500);
+    }
+
+    private edge22d(e: Edge): Edge2d {
+        const from = this.nodes[e.from];
+        const to = this.nodes[e.to];
+        return new Edge2d(from.x, to.x, from.y, to.y);
     }
 }
 
@@ -25,4 +43,13 @@ class Node2d {
         public x: number,
         public y: number
     ) {}
+}
+
+class Edge2d {
+    constructor (
+        public fromX: number,
+        public toX: number,
+        public fromY: number,
+        public toY: number
+     ) {}
 }
